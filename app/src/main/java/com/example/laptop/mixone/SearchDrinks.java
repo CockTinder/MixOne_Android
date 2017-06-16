990e34c3c06f3fbf9ad5ac93ae36e75d5bba48ec
aG0TH3 <anton.gothe@gmail.com>
2017-06-16 11:55:27 +0200

    added SearchDrinks Activity

---
+++ app/src/main/java/com/example/laptop/mixone/SearchDrinks.java
@@ -0,0 +1,13 @@
+package com.example.laptop.mixone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchDrinks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_drinks);
+    }
+}
