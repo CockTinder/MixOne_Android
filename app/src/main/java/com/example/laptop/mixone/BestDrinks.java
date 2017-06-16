702b42c6339243c164a4bc6653d4dcd0baedead3
marcsinner <guitarmax10@gmail.com>
2017-06-16 19:49:25 +0200

    bug fixes

--- app/src/main/java/com/example/laptop/mixone/BestDrinks.java
+++ app/src/main/java/com/example/laptop/mixone/BestDrinks.java
@@ -2,6 +2,7 @@ package com.example.laptop.mixone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BestDrinks extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class BestDrinks extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_drinks);
        // getActionBar().hide();
+    }
+
+    public void sendMessage(View view) {
+
     }
 }
--- app/src/main/java/com/example/laptop/mixone/SearchDrinks.java
+++ app/src/main/java/com/example/laptop/mixone/SearchDrinks.java
@@ -1,14 +1,26 @@
 package com.example.laptop.mixone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SearchDrinks extends AppCompatActivity {
+
+    private Button mBtLaunchActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_best_drinks);
        // getActionBar().hide();
+
     }
+
+    public void sendMessage(View view) {
+
+    }
+
+
 }
