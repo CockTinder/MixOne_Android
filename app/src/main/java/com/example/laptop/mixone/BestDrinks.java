ed6ba824170054f264c9bed295bf48ff0bdb19ab
marcsinner <guitarmax10@gmail.com>
2017-06-16 17:08:34 +0200

    search drinks verbessert

--- app/src/main/java/com/example/laptop/mixone/BestDrinks.java
+++ app/src/main/java/com/example/laptop/mixone/BestDrinks.java
@@ -8,6 +8,6 @@ public class BestDrinks extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_best_drinks);
+        setContentView(R.layout.activity_search_drinks);
     }
 }
--- app/src/main/java/com/example/laptop/mixone/SearchDrinks.java
+++ app/src/main/java/com/example/laptop/mixone/SearchDrinks.java
@@ -8,6 +8,6 @@ public class SearchDrinks extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_drinks);
+        setContentView(R.layout.activity_best_drinks);
     }
 }
