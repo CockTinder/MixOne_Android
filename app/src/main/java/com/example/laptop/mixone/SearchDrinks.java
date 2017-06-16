f24476003ce85e78fbdb90d5a5e20f9a2aa2830d
aG0TH3 <anton.gothe@gmail.com>
2017-06-16 21:30:28 +0200

    bug fixes

--- app/src/main/java/com/example/laptop/mixone/SearchDrinks.java
+++ app/src/main/java/com/example/laptop/mixone/SearchDrinks.java
@@ -5,10 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-<<<<<<< HEAD
-=======
 import android.view.View.*;
->>>>>>> 4ebe5d62bd4a046b8e5f7968c989909f1b650bea
 
 public class SearchDrinks extends AppCompatActivity {
 
@@ -19,16 +16,13 @@ public class SearchDrinks extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_best_drinks);
        // getActionBar().hide();
-<<<<<<< HEAD
 
-=======
         final Button btn = (Button)findViewById(R.id.button2);
         btn.setOnClickListener(new OnClickListener(){
             public void onClick(View v){
                 startActivity(new Intent(SearchDrinks.this, BestDrinks.class));
             }
         });
->>>>>>> 4ebe5d62bd4a046b8e5f7968c989909f1b650bea
     }
 
     public void sendMessage(View view) {
