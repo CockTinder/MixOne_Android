07428f659fbb8e31b1a91172f98a389f7d24fba2
aG0TH3 <anton.gothe@gmail.com>
2017-06-16 23:04:26 +0200

    added working activity change buttton

--- app/src/main/java/com/example/laptop/mixone/BestDrinks.java
+++ app/src/main/java/com/example/laptop/mixone/BestDrinks.java
@@ -1,5 +1,6 @@
 package com.example.laptop.mixone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +10,12 @@ public class BestDrinks extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_drinks);
+        setContentView(R.layout.activity_best_drinks);
        // getActionBar().hide();
     }
 
     public void sendMessage(View view) {
-
+        Intent intent = new Intent(this, SearchDrinks.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/laptop/mixone/SearchDrinks.java
+++ app/src/main/java/com/example/laptop/mixone/SearchDrinks.java
@@ -6,27 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.*;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class SearchDrinks extends AppCompatActivity {
-
-    private Button mBtLaunchActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_best_drinks);
+        setContentView(R.layout.activity_search_drinks);
        // getActionBar().hide();
-
-        final Button btn = (Button)findViewById(R.id.button2);
-        btn.setOnClickListener(new OnClickListener(){
-            public void onClick(View v){
-                startActivity(new Intent(SearchDrinks.this, BestDrinks.class));
-            }
-        });
     }
 
     public void sendMessage(View view) {
-
+       Button button = (Button) view;
+        button.setText("clicked");
     }
 
 
